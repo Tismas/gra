22fabdf7a7117702e229762133603da1fc6e00b7
pawel <pbroniarczyk@outlook.com>
2018-11-24 16:13:37 +0100

    directory cleaned with map & navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Nav extends Component {
+  render() {
+    return (
+      <div>
+        Navbar with 3 icons
+      </div>
+    )
+  }
+}
+
+export default Nav;
